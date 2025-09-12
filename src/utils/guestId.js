2512eb6fbefca52913cf358ee082529c6b362bbb
Kovid Kunduru <kovid@Kovids-MacBook-Air.local>
2025-09-13 02:36:58 +1000

    guestId.js changes

--- src/utils/guestId.js
+++ src/utils/guestId.js
@@ -1,7 +1,9 @@
 export const getOrCreateGuestId = () => {
   let guestId = localStorage.getItem("guestId");
   if (!guestId) {
-    guestId = crypto.randomUUID();
+    // guestId = crypto.randomUUID();
+    guestId = window.crypto.randomUUID();
+
     localStorage.setItem("guestId", guestId);
   }
   return guestId;
